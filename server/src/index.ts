6d679ccf38cb13948f9495644ec009c8307d5416
Leonardo Bueno <leobuenoraf@gmail.com>
2026-01-31 17:15:34 -0300

    Testando conexão com banco de dados

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (_req, res) => {
+  res.send('API Hunter rodando 🚀')
+})
+
+app.listen(3333, () => {
+  console.log('Server rodando na porta 3333')
+})
